c230ddc6159b2ba65bfc777c96fe26ab02caf50e
john gallagher <127highstreet@gmail.com>
2022-03-15 16:17:59 -0400

    saving

---
+++ Gifter/Repositories/IPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using Gifter.Models;
+using System.Collections.Generic;
+
+namespace Gifter.Repositories
+{
+    public interface IPostRepository
+    {
+        void Add(Post post);
+        void Delete(int id);
+        List<Post> GetAll();
+        Post GetById(int id);
+        void Update(Post post);
+        List<Post> GetAllWithComments();
+        Post GetPostByIdWithComments(int id);
+        List<Post> Search(string criterion, bool sortDescending);
+    }
+}
